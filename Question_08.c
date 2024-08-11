a1975e755580ad4457a1c771c250d685ea1496f3
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-08-12 00:00:24 +0530

    Question 08

---
+++ Question_08.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define PI 3.14159
+
+//kavindu
+//ec-22053
+//galkotu-ec22053@stu.kln.ac.lk
+
+int main(){
+printf("%d\n",3+4-7*8/5%10);
+printf("%f\n",3/2+3*8/3-3+1.5/3);
+
+return 0;
+}
