54bc3593b66b9ca0f34133ab37b5b39326a99a12
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-08-11 23:58:53 +0530

    Question 03

---
+++ Question_03.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//kavindu
+//ec-22053
+//galkotu-ec22053@stu.kln.ac.lk
+int main(){
+/*
+ Write a C program to find the results of the following expressions. Display the result of each expression
+separately.
+201 % 12
+122 % 3
+5005 % 103
+100005 % 23
+2085 % 13
+Upload your completed C program into the Lab 04 – Program 03 submission folder.
+*/
+printf("201 % 12 is %d\n",201%12);
+printf("122 % 3 is %d\n",122%3);
+printf("5005 % 103 is %d\n",5005%103);
+printf("100005 % 23 is %d\n",100005% 3);
+printf("2085 % 13 %d\n",2085%13);
+return 0;
+}
